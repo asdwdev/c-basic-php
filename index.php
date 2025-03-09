468e861e90a6bf73febeef687d8df454869288a3
asdw <asdwdev@gmail.com>
2025-03-10 01:07:08 +0700

    si paling variabel

--- index.php
+++ index.php
@@ -1,8 +1,9 @@
 <?php
-// nyetak versi php 
-// phpinfo()
-
-// ini adalah komentar 
-echo 'hello world!'; // ; buat nandain baris kodenya udah selesai ditulis
-// kalau punya satu baris kode, gak perlu pakai titik koma
-echo 'jandi suki';
+// variabel
+$x = 100;
+$y = 50;
+$result = $x + $y;
+echo $result;
+echo '<br>';
+$nama_lengkap = 'jandi suki suki desu cuyy ma broo';
+echo $nama_lengkap;
