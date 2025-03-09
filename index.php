1e06c2cb69fe994fc8ed45539d2d8b4797603640
asdw <asdwdev@gmail.com>
2025-03-10 00:57:55 +0700

    komsmits pertstama

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+// nyetak versi php 
+// phpinfo()
+
+// ini adalah komentar 
+echo 'hello world!'; // ; buat nandain baris kodenya udah selesai ditulis
+// kalau punya satu baris kode, gak perlu pakai titik koma
+echo 'jandi suki';
